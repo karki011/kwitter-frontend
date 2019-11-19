26ca7bcbc643f8bb7fdeee81f556e0693e2c542c
Binup karki <karki011@gmail.com>
2019-11-19 15:50:45 -0600

    created messagetextbox component and css file

---
+++ src/react/components/MessageTextBox.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Form, TextArea, Button } from 'semantic-ui-react';
+import './MessageTextBox.css';
+
+class MessageTextBox extends Component {
+	state = {};
+
+	render() {
+		return (
+			<Form className="form-control">
+				<Form.Field control={TextArea} label="Create Post" placeholder="What's on your mind? " />
+				<Form.Field className="postBtn" control={Button}>
+					Post
+				</Form.Field>
+			</Form>
+		);
+	}
+}
+
+export default MessageTextBox;
--- src/react/components/index.js
+++ src/react/components/index.js
@@ -1,7 +1,8 @@
 // components from own project
-export { default as LoginForm } from "./LoginForm";
-export { default as Menu } from "./Menu";
+export { default as LoginForm } from './LoginForm';
+export { default as Menu } from './Menu';
+export { default as MessageTextBox } from './MessageTextBox';
 
 // components from other packages
-export { Switch, Route, Link } from "react-router-dom";
-export { default as Spinner } from "react-spinkit";
+export { Switch, Route, Link } from 'react-router-dom';
+export { default as Spinner } from 'react-spinkit';
