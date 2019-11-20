14e69a56b53212e2ebaeab59472e80b04fa219ed
Binup karki <karki011@gmail.com>
2019-11-19 22:37:36 -0600

    login page to semantic- ui

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { LoginForm, Menu } from "../components";
+import { LoginForm } from "../components";
 import { userIsNotAuthenticated } from "../HOCs";
 
 class Home extends React.Component {
   render() {
     return (
       <>
-        <Menu />
-        <h2>Your favorite microblogging platform</h2>
+        {/* <Menu /> */}
         <LoginForm />
       </>
     );
