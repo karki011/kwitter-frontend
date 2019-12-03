aa4e1b17ed4a391ed51b500e881db7ee0ec0deb5
Eugene Lee <eugenelee905@gmail.com>
2019-12-02 21:50:53 -0500

    Finished deleter user button

--- src/react/pages/Profile.js
+++ src/react/pages/Profile.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { Menu, MessageTextBox, MessageList, RecentMessageFeed } from '../components';
-import { userIsAuthenticated } from '../HOCs';
-import { Grid } from 'semantic-ui-react';
+import React from "react";
+import {
+  Menu,
+  MessageTextBox,
+  MessageList,
+  RecentMessageFeed
+} from "../components";
+import { userIsAuthenticated } from "../HOCs";
+import { Grid } from "semantic-ui-react";
 
 class Profile extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Grid celled="internally">
-					<Grid.Row>
-						<Grid.Column width={3}>
-							<Menu isAuthenticated={this.props.isAuthenticated} />
-						</Grid.Column>
-						<Grid.Column width={10}>
-							<MessageTextBox />
-							<MessageList username={this.props.match.params.username} />
-						</Grid.Column>
-						<Grid.Column width={3}>
-							<RecentMessageFeed />
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-				{/* <UserCard username={this.props.match.params.username} /> */}
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Grid celled="internally">
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Menu isAuthenticated={this.props.isAuthenticated} />
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <MessageTextBox />
+              <MessageList username={this.props.match.params.username} />
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <RecentMessageFeed />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        {/* <UserCard username={this.props.match.params.username} /> */}
+      </React.Fragment>
+    );
+  }
 }
 
 export default userIsAuthenticated(Profile);
--- src/react/pages/UserProfileEdit.js
+++ src/react/pages/UserProfileEdit.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Menu, RecentMessageFeed, UserCard } from '../components';
-import { userIsAuthenticated } from '../HOCs';
-import { Grid } from 'semantic-ui-react';
+import React from "react";
+import {
+  Menu,
+  RecentMessageFeed,
+  UserCard,
+  DeleteUserButton
+} from "../components";
+import { userIsAuthenticated } from "../HOCs";
+import { Grid } from "semantic-ui-react";
 
 class UserProfileEdit extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Grid celled="internally">
-					<Grid.Row>
-						<Grid.Column width={3}>
-							<Menu isAuthenticated={this.props.isAuthenticated} />
-						</Grid.Column>
-						<Grid.Column width={10}>
-							<UserCard username="testdude" />
-						</Grid.Column>
-						<Grid.Column width={3}>
-							<RecentMessageFeed />
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Grid celled="internally">
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Menu isAuthenticated={this.props.isAuthenticated} />
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <UserCard username="testdude" />
+              <DeleteUserButton />
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <RecentMessageFeed />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
 
 export default userIsAuthenticated(UserProfileEdit);
