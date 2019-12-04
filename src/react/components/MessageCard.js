0ed76c6d895d966f11d0155af2dc8d959c243648
dbmiddle <dbmiddle39@gmail.com>
2019-12-04 15:55:50 -0500

    added some styling to the message cards

--- src/react/components/MessageCard.js
+++ src/react/components/MessageCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withAsyncAction } from "../HOCs";
-import { Button } from "semantic-ui-react";
+import { Button, Card, Image } from "semantic-ui-react";
+import "./MessageCard.css";
 
 class MessageCard extends React.Component {
   // componentDidMount(){
@@ -8,19 +9,21 @@ class MessageCard extends React.Component {
   // }
   render() {
     return (
-      <div
-        style={{
-          border: "1px solid black",
-          padding: "1em",
-          margin: "2em",
-          borderRadius: "10px"
-        }}
-      >
-        <h4>{this.props.username}</h4>
-        <p>{this.props.text}</p>
-        <p>{new Date(this.props.createdAt).toDateString()}</p>
+      
+      <div id = "messageCardContainer">
+      <Card id = "messageCard">
+        <Card.Content>
+          <Image
+            floated='right'
+            size='mini'
+            src="http://simpleicon.com/wp-content/uploads/user1.svg"
+          />
+        <Card.Header><h4>{this.props.username}</h4></Card.Header>
+        <Card.Description><p>{this.props.text}</p></Card.Description>
+        <br></br>
+        <Card.Content><p>{new Date(this.props.createdAt).toDateString()}</p></Card.Content>
         {/* <p>Likes: {this.props.message.likes.length}</p> */}
-
+        <br></br>
         {this.props.currentUser === this.props.username && (
           <Button
             color = "red"
@@ -32,7 +35,10 @@ class MessageCard extends React.Component {
             Delete Message
           </Button>
         )}
-      </div>
+</Card.Content>
+</Card>
+</div>
+      
     );
   }
 }
