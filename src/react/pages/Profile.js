b77ef7476d1cb67a48dc2f0b69093ba5127e4219
Eugene Lee <eugenelee905@gmail.com>
2019-12-04 21:12:36 -0500

    Navigation styling fix

--- src/react/pages/Profile.js
+++ src/react/pages/Profile.js
@@ -8,36 +8,35 @@ import {
 import { userIsAuthenticated } from "../HOCs";
 import { Grid } from "semantic-ui-react";
 
-
 class Profile extends React.Component {
-
-  
-
-	render() {
-		return (
-			<React.Fragment>
-				<Grid celled="internally">
-					<Grid.Row>
-						<Grid.Column width={3}>
-							<Menu isAuthenticated={this.props.isAuthenticated} />
-						</Grid.Column>
-						<Grid.Column width={10}>
-              <MessageTextBox 
-              handleChange= {this.props.handleChange}
-			  value={this.props.text}
-              postMessage={this.props.handleComposeMessage} 
+  render() {
+    return (
+      <React.Fragment>
+        <Grid celled="internally">
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Menu
+                isAuthenticated={this.props.isAuthenticated}
+                username={JSON.parse(localStorage.login).result.username}
               />
-							<MessageList username={this.props.username} />
-						</Grid.Column>
-						<Grid.Column width={3}>
-							<RecentMessageFeed />
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
-				{/* <UserCard username={this.props.match.params.username} /> */}
-			</React.Fragment>
-		);
-	}
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <MessageTextBox
+                handleChange={this.props.handleChange}
+                value={this.props.text}
+                postMessage={this.props.handleComposeMessage}
+              />
+              <MessageList username={this.props.username} />
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <RecentMessageFeed />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        {/* <UserCard username={this.props.match.params.username} /> */}
+      </React.Fragment>
+    );
+  }
 }
 
 export default userIsAuthenticated(Profile);
